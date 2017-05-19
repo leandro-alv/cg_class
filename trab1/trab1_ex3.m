6a3780ee553bdb920f552821f408f4ba3f847645
leandro-alv <leandro.rezende.alves@gmail.com>
2017-05-18 22:51:21 -0300

    Update ex3 and ex5

--- trab1/trab1_ex3.m
+++ trab1/trab1_ex3.m
@@ -1,5 +1,14 @@
 pkg load image
 
-w = [1/9, 1/9, 1/9; 1/9, 1/9, 1/9; 1/9, 1/9, 1/9];
+lion = imread("lion.png");
+woman = imread("woman.png");
 
-f = 
+f = [1/9 1/3 1/6 ; 1/4 1/2 1/8 ; 1/7 1/5 1/7];
+
+lion_fil = imfilter(lion, f);
+lion_comb = [lion lion_fil];
+imwrite(lion_comb, "lion_comb_custom.png");
+
+woman_fil = imfilter(woman, f);
+woman_comb = [woman woman_fil];
+imwrite(woman_comb, "woman_comb_custom.png");
--- trab1/trab1_ex5.m
+++ trab1/trab1_ex5.m
@@ -2,16 +2,13 @@ pkg load image
 
 coin = imread("Coins.png");
 
-coin_lim = coin;
+imhist(coin);
 
-for i = 1:rows(coin)
-  for j = 1:columns(coin)
-    if (coin(i,j) < 128)
-      coin_lim(i,j) = 128
-    else
-      coin_lim(i,j) = coin(i,j)  
-    endif
-  endfor
-endfor
+lim = 90;
 
-subplot(1,2,1), imshow(coin), subplot(1,2,2), imshow(coin_lim);
+mask = (coin > lim);
+
+coin_lim = (coin.* mask);
+
+coin_comb = [coin coin_lim];
+imwrite(coin_comb, "coin_comb_lim.png");
