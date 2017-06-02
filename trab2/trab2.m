9a6126f2f9c7a52b3d596505a572cbe50ae53a9e
leandro-alv <leandro.rezende.alves@gmail.com>
2017-06-02 13:18:01 -0300

    Added centred rotation

--- trab2/trab2.m
+++ trab2/trab2.m
@@ -4,32 +4,32 @@ function rotObj (degrees, matrix)
   axis([-5 5 -5 5], "square"); # Limit the window plot
   
   rad = degrees * (pi/180); # Converts degrees to radians
+  px = 0 - ((max(matrix(1,:)) - min(matrix(1,:))) / 2.0);
+  py = 0 - ((max(matrix(2,:)) - min(matrix(2,:))) / 2.0);
   
   # Rotation matrix
+  transl = [1 0 px;
+            0 1 py;
+            0 0 1];
+
   rot = [cos(rad) -sin(rad) 0;
          sin(rad)  cos(rad) 0;
          0         0        1];
-         
-  rot_obj = rot * matrix; # Rotates the object
+  
+  rot_obj = (inv(transl) * rot * transl) * matrix;
   
   # Plotting the matrix
-  plotObj(matrix)
-  plotObj(rot_obj)
+  plotObj(matrix);
+  plotObj(rot_obj);
 endfunction
 
 function plotObj (matrix)
   hold on;
   
-  for i = 1:size(matrix,2)
-    plot(matrix(1,i), matrix(2,i));
-    if (i < size(matrix,2))
-      line([matrix(1,i), matrix(1,i+1)],
-           [matrix(2,i), matrix(2,i+1)]);
-    else
-      line([matrix(1,i), matrix(1,1)],
-           [matrix(2,i), matrix(2,1)]);
-    endif  
-  endfor
+  i = size(matrix,2);
+  line([matrix(1,:)], [matrix(2,:)]);
+  line([matrix(1,i), matrix(1,1)],
+       [matrix(2,i), matrix(2,1)]);
   
   hold off;
 endfunction
